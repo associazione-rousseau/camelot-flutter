50ed78fd1852bdc95304d8d84b841e73799324f8
Mattia Panzeri <mattia.panzeri93@gmail.com>
2019-11-07 00:56:25 +0100

    Refactor 'LoggedScreen' widget and add 'LoadingScreen' check

--- rousseau_vote/lib/src/widgets/logged_screen.dart
+++ rousseau_vote/lib/src/widgets/logged_screen.dart
@@ -1,19 +1,32 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 import 'package:rousseau_vote/src/providers/login.dart';
 import 'package:rousseau_vote/src/screens/login_screen.dart';
+
+import './loading_screen.dart';
 
 // Widget to use for loggedin screens. It checks if the user is logged in. If
 // not it redirects to the login screen.
 class LoggedScreen extends StatelessWidget {
   final Widget screen;
+
   const LoggedScreen(this.screen);
 
   @override
   Widget build(BuildContext context) {
-    final isLoggedIn = Provider.of<Login>(context).isLoggedIn();
-    return isLoggedIn ? screen : LoginScreen();
+    final loginContext = Provider.of<Login>(context);
+
+    if (loginContext.isLoading()) {
+      return LoadingScreen(
+        message: 'Logging In',
+      );
+    }
+
+    if (!loginContext.isLoggedIn()) {
+      return LoginScreen();
+    }
+
+    return screen;
   }
 }
