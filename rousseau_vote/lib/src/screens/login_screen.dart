a0ee1d3bac8b98f2e99e88b922546876f4b2f33c
Maurizio <mfmanca2000@hotmail.com>
2019-11-07 00:08:53 +0100

    Removed unused import

--- rousseau_vote/lib/src/screens/login_screen.dart
+++ rousseau_vote/lib/src/screens/login_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:rousseau_vote/src/widgets/rousseau_logged_scaffold.dart';
 
 class LoginScreen extends StatelessWidget {
   static const routeName = '/login';
