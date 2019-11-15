4d2e59c4b70b4f26c293377bec3c30acb2ac560c
Emanuel Mazzilli <emanuelconunaemme@hotmail.it>
2019-11-14 17:07:59 -0800

    Fix tests

--- rousseau_vote/test/main_test.dart
+++ rousseau_vote/test/main_test.dart
@@ -5,7 +5,6 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:rousseau_vote/main.dart';
@@ -15,16 +14,6 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(RousseauVoteApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.byType(RousseauVoteApp), findsOneWidget);
   });
 }
