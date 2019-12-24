ce5279edb278522607d36d7ee7c2dca60a9cebba
Cristian Laurini <cristian.laurini@gmail.com>
2019-12-24 12:25:14 +0100

    added info button to poll-card

--- rousseau_vote/lib/src/widgets/poll_card.dart
+++ rousseau_vote/lib/src/widgets/poll_card.dart
@@ -5,8 +5,6 @@ import 'package:rousseau_vote/src/l10n/rousseau_localizations.dart';
 import 'package:rousseau_vote/src/models/poll.dart';
 import 'package:rousseau_vote/src/util/ui_util.dart';
 
-import 'link_flat_button.dart';
-
 class PollCard extends StatelessWidget {
   const PollCard(this._poll);
 
@@ -23,48 +21,62 @@ class PollCard extends StatelessWidget {
     final PollStatus pollStatus = _poll.calculatePollStatus();
     final Color statusColor = COLOR_MAPPING[pollStatus];
     return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15.0),
-      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
       elevation: 5,
-      child: Padding(
-        padding: const EdgeInsets.all(15),
-        child: Column(
-          children: <Widget>[
-            ListTile(
-              leading: Icon(Icons.adjust, color: statusColor, size: 50),
-              title: Text(
-                _poll.title,
-                textAlign: TextAlign.center,
+      child: InkWell(
+        customBorder: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.adjust, color: statusColor, size: 50),
+                title: Text(
+                  _poll.title,
+                  textAlign: TextAlign.center,
+                ),
+                subtitle: Column(
+                  children: <Widget>[
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(RousseauLocalizations.getText(context, 'poll-start')),
+                        Text(
+                          '${UiUtil.formatDate( _poll.voteStartingDate)}',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(RousseauLocalizations.getText(context, 'poll-end')),
+                        Text(
+                          '${UiUtil.formatDate(_poll.voteEndingDate)}',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
-              subtitle: Column(
-                children: <Widget>[
-                  const SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(RousseauLocalizations.getText(context, 'poll-start')),
-                      Text(
-                        '${UiUtil.formatDate( _poll.voteStartingDate)}',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      )
-                    ],
+              _poll.announcementLink != null
+              ? OutlineButton.icon(
+                  borderSide: BorderSide(color: statusColor),
+                  textColor: statusColor,
+                  icon: Icon(Icons.info_outline),
+                  onPressed: () => null,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(RousseauLocalizations.getText(context, 'poll-end')),
-                      Text(
-                        '${UiUtil.formatDate(_poll.voteEndingDate)}',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ),
-          ]
-        )
+                  label: Text(RousseauLocalizations.getText(context, 'poll-info')),
+                )
+              : Container()
+            ]
+          )
+        ),
+        onTap: () => null,
       )
           // ButtonBarTheme(
           //   data: const ButtonBarThemeData(alignment: MainAxisAlignment.end),
