2255bbe1e98f156f56ec6df0c657b66a68c8fae1
Cristian Laurini <cristian.laurini@gmail.com>
2019-12-25 19:59:50 +0100

    constants: fix CLOSED_RED color

--- rousseau_vote/lib/src/config/app_constants.dart
+++ rousseau_vote/lib/src/config/app_constants.dart
@@ -27,7 +27,7 @@ const Color BACKGROUND_GREY = Color(0xFFECEFF1);
 
 const Color PUBLISHED_ORANGE = Color(0xFFE78853);
 const Color OPEN_GREEN = Color(0xFF46B29C);
-const Color CLOSED_RED = Color(0xdFFF5D4B);
+const Color CLOSED_RED = Color(0xFFDF5D4B);
 
 //images
 const AssetImage WHITE_LOGO = AssetImage('assets/images/rousseau_white.png');
