edb40a045f74a973100a36fbbc92fff1e8c3e22c
emanuelconunaemme <emanuelconunaemme@hotmail.it>
2020-07-10 21:59:11 -0700

    Use CurrentUserProvider for CurrentUserCard

--- rousseau_vote/lib/src/widgets/user/current_user_card.dart
+++ rousseau_vote/lib/src/widgets/user/current_user_card.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:provider/provider.dart';
 import 'package:rousseau_vote/src/injection/injector_config.dart';
 import 'package:rousseau_vote/src/models/user/current_user.dart';
 import 'package:rousseau_vote/src/network/graphql/graphql_queries.dart';
+import 'package:rousseau_vote/src/providers/current_user_provider.dart';
 import 'package:rousseau_vote/src/store/token_store.dart';
 import 'package:rousseau_vote/src/widgets/user/profile_picture.dart';
 
@@ -15,24 +17,25 @@ class CurrentUserCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GraphqlQueryWidget<CurrentUser>(
-        query: currentUserShort,
-        builderSuccess: (CurrentUser currentUser) {
-          return ListTile(
-            leading: ProfilePicture(currentUser.getProfilePictureUrl()),
-            title: Text(
-              currentUser.fullName,
-              style: const TextStyle(fontSize: 20),
-            ),
-            subtitle: Text(currentUser.slug),
-          );
-        },
-        builderLoading: () {
-          return _placeholderWidget();
-        },
-        builderError: (List<GraphQLError> errors) {
-          return _placeholderWidget();
-        });
+    final CurrentUserProvider provider = Provider.of<CurrentUserProvider>(context);
+    final CurrentUser currentUser = provider.getCurrentUser();
+
+    if(currentUser != null) {
+      return ListTile(
+        leading: ProfilePicture(currentUser.getProfilePictureUrl()),
+        title: Text(
+          currentUser.fullName,
+          style: const TextStyle(fontSize: 20),
+        ),
+        subtitle: Text(currentUser.slug),
+      );
+    }
+
+    if (provider.isLoading()) {
+      return _placeholderWidget();
+    }
+
+    return _placeholderWidget();
   }
 
   Widget _placeholderWidget() {
