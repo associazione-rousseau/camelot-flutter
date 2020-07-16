6bad2f914f09031aa279cd5762d6c57a553c2ba5
emanuelconunaemme <emanuelconunaemme@hotmail.it>
2020-07-16 15:28:44 -0700

    Use GraphqlQueryWidget for CurrentUserCard

--- rousseau_vote/lib/src/widgets/user/current_user_card.dart
+++ rousseau_vote/lib/src/widgets/user/current_user_card.dart
@@ -7,41 +7,45 @@ import 'package:rousseau_vote/src/models/user/current_user.dart';
 import 'package:rousseau_vote/src/network/graphql/graphql_queries.dart';
 import 'package:rousseau_vote/src/providers/current_user_provider.dart';
 import 'package:rousseau_vote/src/store/token_store.dart';
+import 'package:rousseau_vote/src/util/ui_util.dart';
 import 'package:rousseau_vote/src/widgets/user/profile_picture.dart';
 
 import '../graphql_query_widget.dart';
 import '../loading_indicator.dart';
+
+const double PROFILE_PICTURE_RADIUS = 30;
 
 class CurrentUserCard extends StatelessWidget {
   const CurrentUserCard();
 
   @override
   Widget build(BuildContext context) {
-    final CurrentUserProvider provider = Provider.of<CurrentUserProvider>(context);
-    final CurrentUser currentUser = provider.getCurrentUser();
-
-    if(currentUser != null) {
-      return ListTile(
-        leading: ProfilePicture(url: currentUser.getProfilePictureUrl()),
-        title: Text(
-          currentUser.fullName,
-          style: const TextStyle(fontSize: 20),
-        ),
-        subtitle: Text(currentUser.slug),
-      );
-    }
-
-    if (provider.isLoading()) {
-      return _placeholderWidget();
-    }
-
-    return _placeholderWidget();
+    return GraphqlQueryWidget<CurrentUser>(
+      query: currentUserShort,
+      builderSuccess: (CurrentUser currentUser) {
+        return ListTile(
+          leading: ProfilePicture(url: currentUser.getProfilePictureUrl(), radius: PROFILE_PICTURE_RADIUS),
+          title: Text(
+            currentUser.fullName,
+            style: const TextStyle(fontSize: 20),
+          ),
+          subtitle: Text(currentUser.slug),
+        );
+      },
+      builderLoading: () {
+        return _placeholderWidget();
+      },
+      builderError: (List<GraphQLError> errors) {
+        showSimpleSnackbar(context, 'error-profile-loading');
+        return _placeholderWidget();
+      },
+    );
   }
 
   Widget _placeholderWidget() {
     final TokenStore tokenStore = getIt<TokenStore>();
     return ListTile(
-      leading: const ProfilePicture(),
+      leading: const ProfilePicture(radius: PROFILE_PICTURE_RADIUS),
       title: Text(
         tokenStore.getUserFullName(),
         style: const TextStyle(fontSize: 20),
