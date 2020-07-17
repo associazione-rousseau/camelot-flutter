9e4ca43f1e6410a1a2da30fc96fa2d392bc8f868
pietro <mini.pietro@associazionerousseau.it>
2020-07-17 11:55:23 +0200

    removed switch case in favor of declarative approach

--- rousseau_vote/lib/src/widgets/user/badge_slider.dart
+++ rousseau_vote/lib/src/widgets/user/badge_slider.dart
@@ -5,12 +5,37 @@ import 'package:rousseau_vote/src/models/badge.dart';
 
 class BadgeSlider extends StatelessWidget {
   const BadgeSlider({this.badges});
-
   final List<Badge> badges;
   static const double ICON_SIZE = 40;
+  static const Map<String, String> BADGE_ICON_MAPPING = { 
+    'list_representative': 'merit_1', 
+    'italia_cinque_stelle_volunteer': 'merit_1',
+    'villaggio_rousseau_volunteer': 'merit_1',
+    'call_to_actions_organizer': 'merit_2',
+    'activism_organizer': 'merit_2',
+    'sharing_proposer': 'merit_2',
+    'user_lex_proposer': 'merit_2',
+    'graduate': 'merit_3',
+    'english_language_expert': 'merit_4',
+    'openday_participant': 'merit_5',
+    'villaggio_rousseau_participant': 'merit_5',
+    'elearnign_student': 'merit_6',
+    'special_mentions': 'merit_7',
+    'high_specialization': 'merit_8',
+    'community_leader': 'merit_9',
+  };
+
   @override
   Widget build(BuildContext context) {
-    final List<String> meritIcons = badgesToMerits();
+
+    final List<String> meritIcons = [];
+    for(final Badge badge in badges) {
+      final String meritIcon = BADGE_ICON_MAPPING[badge.code];
+      if(!meritIcons.contains(meritIcon)) {
+        meritIcons.add(meritIcon);
+      }
+    }
+
     return SizedBox(
       height: 71,
       child: Row(
@@ -53,7 +78,7 @@ class BadgeSlider extends StatelessWidget {
                     borderRadius: BorderRadius.circular(ICON_SIZE / 2),
                     onTap: () => showMeritMeaning(meritIcons[index],context),
                     child: Image( 
-                      image: AssetImage('assets/merits/' + meritIcons[index] +  '_true.png'),
+                      image: AssetImage('assets/images/merits/' + meritIcons[index] +  '_true.png'),
                       width: ICON_SIZE,
                       height: ICON_SIZE,
                     ),
@@ -75,7 +100,7 @@ class BadgeSlider extends StatelessWidget {
           content: Column(
             children: <Widget>[
               Image( 
-                image: AssetImage('assets/merits/' + merit +  '_true.png'),
+                image: AssetImage('assets/images/merits/' + merit +  '_true.png'),
                 width: 60,
                 height: 60,
               ),
@@ -88,29 +113,5 @@ class BadgeSlider extends StatelessWidget {
         );
       }
     );
-  }
-
-  List<String> badgesToMerits(){
-    List<String> meritIcons = [];
-    for(final Badge badge in badges){
-      switch(badge.code) {
-        case 'list_representative':
-        case 'italia_cinque_stelle_volunteer':
-        case 'villaggio_rousseau_volunteer': if(!meritIcons.contains('merit_1')) meritIcons.add('merit_1'); break;
-        case 'call_to_actions_organizer':
-        case 'activism_organizer':
-        case 'sharing_proposer':
-        case 'user_lex_proposer': if(!meritIcons.contains('merit_2')) meritIcons.add('merit_2'); break; 
-        case 'graduate': meritIcons.add('merit_3'); break;
-        case 'english_language_expert': meritIcons.add('merit_4'); break;
-        case 'openday_participant':
-        case 'villaggio_rousseau_participant': if(!meritIcons.contains('merit_5')) meritIcons.add('merit_5'); break;
-        case 'elearnign_student': meritIcons.add('merit_6'); break;
-        case 'special_mentions': meritIcons.add('merit_7'); break;
-        case 'high_specialization': meritIcons.add('merit_8'); break;
-        case 'community_leader': meritIcons.add('merit_9'); break;
-      }
-    }
-    return meritIcons;
   }
 }
