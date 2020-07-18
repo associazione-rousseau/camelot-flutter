c3b8e3b409ec96b8fee9ffae6d1f012a815a133c
emanuelconunaemme <emanuelconunaemme@hotmail.it>
2020-07-17 18:23:44 -0700

    Add badges to CurrentUserCard

--- rousseau_vote/lib/src/widgets/user/current_user_card.dart
+++ rousseau_vote/lib/src/widgets/user/current_user_card.dart
@@ -6,6 +6,8 @@ import 'package:rousseau_vote/src/models/user/current_user.dart';
 import 'package:rousseau_vote/src/network/graphql/graphql_queries.dart';
 import 'package:rousseau_vote/src/store/token_store.dart';
 import 'package:rousseau_vote/src/util/ui_util.dart';
+import 'package:rousseau_vote/src/util/widget/vertical_space.dart';
+import 'package:rousseau_vote/src/widgets/profile/badges_widget.dart';
 import 'package:rousseau_vote/src/widgets/user/profile_picture.dart';
 
 import '../graphql_query_widget.dart';
@@ -21,23 +23,12 @@ class CurrentUserCard extends StatelessWidget {
       query: currentUserShort,
       fetchPolicy: FetchPolicy.cacheFirst,
       builderSuccess: (CurrentUser currentUser) {
-        return InkWell(
-          child: ListTile(
-            leading: ProfilePicture(url: currentUser.getProfilePictureUrl(), radius: PROFILE_PICTURE_RADIUS),
-            title: Text(
-              currentUser.fullName,
-              style: const TextStyle(fontSize: 20),
-            ),
-            subtitle: Text(currentUser.slug),
-            onTap: () {
-              Navigator.of(context).pop();
-              if (currentUser.profile != null) {
-                openProfile(context, currentUser.slug);
-              } else {
-                showSimpleSnackbar(context, 'message-profile-not-compiled');
-              }
-            }
-          ),
+        return Column(
+          children: <Widget>[
+            _identitySection(context, currentUser),
+            const VerticalSpace(10),
+            BadgesWidget(currentUser.badges, 20),
+          ],
         );
       },
       builderLoading: () {
@@ -47,6 +38,27 @@ class CurrentUserCard extends StatelessWidget {
         showSimpleSnackbar(context, 'error-profile-loading');
         return _placeholderWidget();
       },
+    );
+  }
+
+  Widget _identitySection(BuildContext context, CurrentUser currentUser) {
+    return InkWell(
+      child: ListTile(
+          leading: ProfilePicture(url: currentUser.getProfilePictureUrl(), radius: PROFILE_PICTURE_RADIUS),
+          title: Text(
+            currentUser.fullName,
+            style: const TextStyle(fontSize: 20),
+          ),
+          subtitle: Text(currentUser.slug),
+          onTap: () {
+            Navigator.of(context).pop();
+            if (currentUser.profile != null) {
+              openProfile(context, currentUser.slug);
+            } else {
+              showSimpleSnackbar(context, 'message-profile-not-compiled');
+            }
+          }
+      ),
     );
   }
 
