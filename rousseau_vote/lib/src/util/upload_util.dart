754777c8826f429052528589f03b8dd39566bb20
pietro <mini.pietro@associazionerousseau.it>
2020-07-21 11:29:29 +0200

    FileUpload: missing loading url

---
+++ rousseau_vote/lib/src/util/upload_util.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+import 'package:dio/dio.dart';
+import 'package:rousseau_vote/src/injection/injector_config.dart';
+
+Future<String> uploadFile(File file, String fileName) async {
+  final Dio dio = getIt<Dio>();
+  String fileName = file.path.split('/').last;
+  
+  FormData formData = FormData.fromMap(<String,dynamic>{
+    'file': await MultipartFile.fromFile(file.path, filename:fileName),
+  });
+  
+  //TODO missing url
+  dynamic response = await dio.post<dynamic>("/urlToUploadPath", data: formData);
+  return response.data['id'];
+}
--- rousseau_vote/lib/src/widgets/change_residence_button.dart
+++ rousseau_vote/lib/src/widgets/change_residence_button.dart
@@ -1,16 +1,17 @@
 import 'dart:collection';
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:rousseau_vote/src/injection/injector_config.dart';
 import 'package:rousseau_vote/src/models/italianGeographicalDivision.dart';
+import 'package:rousseau_vote/src/models/user/current_user.dart';
 import 'package:rousseau_vote/src/network/handlers/user_network_handler.dart';
 import 'package:rousseau_vote/src/widgets/rounded_button.dart';
 import 'package:rousseau_vote/src/l10n/rousseau_localizations.dart';
+import 'package:rousseau_vote/src/util/upload_util.dart';
 
 class ChangeResidenceButton extends StatelessWidget {  
-  ChangeResidenceButton(this.enabled, this.selectedDivisions, this.overseaseCity);
+  ChangeResidenceButton(this.enabled, this.selectedDivisions, this.overseaseCity, this.userSlug);
   
   File _image;
   ImagePicker picker = ImagePicker();
@@ -19,6 +20,7 @@ class ChangeResidenceButton extends StatelessWidget {
   final String overseaseCity;
   final UserNetworkHandler _userNetworkHandler = getIt<UserNetworkHandler>();
   final bool enabled;
+  final String userSlug;
 
   @override
   Widget build(BuildContext context) {
@@ -39,17 +41,17 @@ class ChangeResidenceButton extends StatelessWidget {
     File image = await getImage();
     
     //2. upload it 
-    //TODO
+    String documentId = await uploadFile(image, 'residence_request_' + userSlug );
     
-    //3.do mutation
-    _userNetworkHandler.createResidenceRequestChange(
+    //3.change residence mutation
+    CurrentUser cu = await _userNetworkHandler.createResidenceRequestChange(
       selectedDivisions['country'].code,
       selectedDivisions['regione'].code,
       selectedDivisions['provincia'].code,
       selectedDivisions['comune'].code,
       selectedDivisions['municipio'] != null ? selectedDivisions['municipio'].code : '',
       overseaseCity ?? '',
-      'documentId'
+      documentId
     );
   }
 
