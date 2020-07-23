dd9d2a9294aad8a563bb912ac2078362c0170a08
pietro <mini.pietro@associazionerousseau.it>
2020-07-23 15:12:54 +0200

    loginInfo button disabled if no change

--- rousseau_vote/lib/src/screens/account/login_info_screen.dart
+++ rousseau_vote/lib/src/screens/account/login_info_screen.dart
@@ -1,11 +1,6 @@
-import 'dart:collection';
 import 'package:flutter/material.dart';
-import 'package:rousseau_vote/src/widgets/label_value.dart';
-import 'package:rousseau_vote/src/widgets/rounded_button.dart';
-import 'package:rousseau_vote/src/network/graphql/graphql_mutations.dart';
+import 'package:rousseau_vote/src/widgets/login_info_widget.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
-import 'package:rousseau_vote/src/injection/injector_config.dart';
-import 'package:rousseau_vote/src/util/ui_util.dart';
 import 'package:rousseau_vote/src/widgets/loading_indicator.dart';
 import 'package:rousseau_vote/src/l10n/rousseau_localizations.dart';
 import 'package:rousseau_vote/src/models/user/current_user.dart';
@@ -18,8 +13,6 @@ class LoginInfoScreen extends StatelessWidget {
   final GlobalKey _scaffoldState = GlobalKey<ScaffoldState>();
 
   static const String ROUTE_NAME = '/account_login_info';
-  TextEditingController _emailController;
-  TextEditingController _phoneNumberController;
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +27,7 @@ class LoginInfoScreen extends StatelessWidget {
       body: GraphqlQueryWidget<CurrentUser>(
         query: currentUserFull,
         builderSuccess: (CurrentUser currentUser) {
-          return _currentUserBody(currentUser,context);
+          return LoginInfoWidget(currentUser.email,currentUser.phoneNumber, _scaffoldState);
         },
         builderLoading: () {
           return const LoadingIndicator();
@@ -44,68 +37,5 @@ class LoginInfoScreen extends StatelessWidget {
         },
       ),
     );
-  }
-
-  Widget _currentUserBody(CurrentUser currentUser, BuildContext context) {
-    //todo remove mutation and create separated widget
-    return Column(
-        children: <Widget>[
-          Expanded(
-            child: ListView(
-              children: <Widget>[
-                LabelValue(_emailController = TextEditingController(text: currentUser.email), 'Email', true),
-                LabelValue(_phoneNumberController = TextEditingController(text: currentUser.phoneNumber), 'Numero di telefono', true),
-              ],
-            ),
-          ),
-          GraphQLProvider(
-            client: getIt<ValueNotifier<GraphQLClient>>(),
-            child: Mutation(
-              options: MutationOptions(
-                documentNode: gql(userAccessDataUpdate),
-                update: (Cache cache, QueryResult result) {
-                  String message;
-
-                  if (result.hasException) {
-                    print('exception');
-                  }
-
-                  final Map<String, Object> user = (result.data
-                      as Map<String, Object>)['user'] as Map<String, Object>;
-                  final LazyCacheMap map = user.values.first;
-                  final List<Object> errors = map.values.first;
-                  message = errors == null || errors.isEmpty
-                      ? 'info-saved'
-                      : 'error-generic';
-
-                  showRousseauSnackbar(context, _scaffoldState, message);
-                },
-              ),
-              builder: (RunMutation runMutation, QueryResult result) {
-                return Padding(
-                  padding: EdgeInsets.all(10),
-                  child: RoundedButton(
-                    text: "SALVA",
-                    loading: result.loading,
-                    onPressed: () {
-                      Map<String, dynamic> variables =
-                          HashMap<String, dynamic>();
-                      variables.putIfAbsent(
-                          'user',
-                          () => {
-                                'phoneNumber': _phoneNumberController.text,
-                                'email': _emailController.text
-                              });
-                      return runMutation(
-                        variables,
-                      );
-                    },
-                  ),
-                );
-              },
-            ),
-          )
-        ],
-      );
   }
 }
