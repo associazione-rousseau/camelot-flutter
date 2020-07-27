9895ea56404552a08d212cf988c48a685bb24140
pietro <mini.pietro@associazionerousseau.it>
2020-07-27 15:36:07 +0200

    removed unnecessary var

--- rousseau_vote/lib/src/util/upload_util.dart
+++ rousseau_vote/lib/src/util/upload_util.dart
@@ -4,13 +4,13 @@ import 'package:rousseau_vote/src/injection/injector_config.dart';
 
 Future<String> uploadFile(File file, String fileName) async {
   final Dio dio = getIt<Dio>();
-  String fileName = file.path.split('/').last;
   
   FormData formData = FormData.fromMap(<String,dynamic>{
     'file': await MultipartFile.fromFile(file.path, filename:fileName),
   });
   
   //TODO missing url
+  
   dynamic response = await dio.post<dynamic>("/urlToUploadPath", data: formData);
   return response.data['id'];
 }
