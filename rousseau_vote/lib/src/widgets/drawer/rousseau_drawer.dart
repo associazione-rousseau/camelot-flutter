709a9290b816eb9cc277293f0ca058016978852f
emanuelconunaemme <emanuelconunaemme@hotmail.it>
2020-08-01 16:02:05 -0700

    Pull GraphqlWidget up in the drawer

--- rousseau_vote/lib/src/widgets/drawer/rousseau_drawer.dart
+++ rousseau_vote/lib/src/widgets/drawer/rousseau_drawer.dart
@@ -1,24 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:rousseau_vote/src/config/links.dart';
+import 'package:rousseau_vote/src/models/user/current_user.dart';
+import 'package:rousseau_vote/src/network/graphql/graphql_queries.dart';
 import 'package:rousseau_vote/src/providers/login.dart';
 import 'package:rousseau_vote/src/screens/blog_screen.dart';
 import 'package:rousseau_vote/src/screens/edit_account_screen.dart';
 import 'package:rousseau_vote/src/screens/feedback_screen.dart';
 import 'package:rousseau_vote/src/screens/polls_screen.dart';
 import 'package:rousseau_vote/src/util/ui_util.dart';
+import 'package:rousseau_vote/src/util/verify_identity_util.dart';
 import 'package:rousseau_vote/src/widgets/drawer/drawer_item.dart';
 import 'package:rousseau_vote/src/widgets/drawer/rousseau_drawer_header.dart';
+import 'package:rousseau_vote/src/widgets/graphql_query_widget.dart';
 
 class RousseauDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return GraphqlQueryWidget<CurrentUser>(
+        query: currentUserShort,
+        fetchPolicy: FetchPolicy.networkOnly,
+        builderSuccess: (CurrentUser currentUser) => _drawer(context, currentUser: currentUser),
+        builderLoading: () => _drawer(context, isLoading: true),
+        builderError: (List<GraphQLError> errors) => _drawer(context, errors: errors)
+    );
+  }
+
+  Widget _drawer(BuildContext context, { CurrentUser currentUser, bool isLoading = false, List<GraphQLError> errors }) {
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
         children: <Widget>[
-          Container(height: 230,child: RousseauDrawerHeader()),
+          Container(height: 230,child: RousseauDrawerHeader(currentUser: currentUser, isLoading: isLoading, errors: errors,)),
           DrawerItem(
             textKey: 'drawer-vote',
             iconData: Icons.account_balance,
