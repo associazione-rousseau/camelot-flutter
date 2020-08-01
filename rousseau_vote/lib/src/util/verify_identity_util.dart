976db90be5bd6879a9d0cf3db63cd46eec35b5dc
emanuelconunaemme <emanuelconunaemme@hotmail.it>
2020-08-01 14:25:28 -0700

    Pull out show verification dialog logic from AskForVerificationWidget

---
+++ rousseau_vote/lib/src/util/verify_identity_util.dart
@@ -0,0 +1,33 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:rousseau_vote/src/injection/injector_config.dart';
+import 'package:rousseau_vote/src/models/user/current_user.dart';
+import 'package:rousseau_vote/src/network/handlers/user_network_handler.dart';
+import 'package:rousseau_vote/src/screens/verify_identity_screen.dart';
+import 'package:rousseau_vote/src/util/ui_util.dart';
+
+import 'dialog_util.dart';
+
+void maybeShowVerificationDialog(BuildContext context) {
+  getIt<UserNetworkHandler>()
+      .fetchCurrentUser(fetchPolicy: FetchPolicy.cacheFirst)
+      .then((CurrentUser currentUser) {
+    if (currentUser.shouldVerifyIdentity) {
+      showVerifyIdentityDialog(context);
+    }
+  });
+}
+
+void showVerifyIdentityDialog(BuildContext context) {
+  showAlertDialog(context,
+      titleKey: 'identity-not-verified',
+      messageKey: 'identity-verification-alert-message',
+      buttonKey1: 'back',
+      buttonKey2: 'identity-verification-verify-now',
+      buttonAction1: () => Navigator.pop(context),
+      buttonAction2: () {
+        Navigator.pop(context);
+        openRoute(context, VerifyIdentityScreen.ROUTE_NAME);
+      });
+}
--- rousseau_vote/lib/src/widgets/ask_for_verification_widget.dart
+++ rousseau_vote/lib/src/widgets/ask_for_verification_widget.dart
@@ -2,13 +2,7 @@
 
 import 'package:after_layout/after_layout.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
-import 'package:rousseau_vote/src/injection/injector_config.dart';
-import 'package:rousseau_vote/src/models/user/current_user.dart';
-import 'package:rousseau_vote/src/network/handlers/user_network_handler.dart';
-import 'package:rousseau_vote/src/screens/verify_identity_screen.dart';
-import 'package:rousseau_vote/src/util/dialog_util.dart';
-import 'package:rousseau_vote/src/util/ui_util.dart';
+import 'package:rousseau_vote/src/util/verify_identity_util.dart';
 
 class AskForVerificationWidget extends StatefulWidget {
   const AskForVerificationWidget({@required this.child});
@@ -29,25 +23,6 @@ class _AskForVerificationWidgetState extends State<AskForVerificationWidget>
 
   @override
   void afterFirstLayout(BuildContext context) {
-    getIt<UserNetworkHandler>()
-        .fetchCurrentUser(fetchPolicy: FetchPolicy.cacheFirst)
-        .then((CurrentUser currentUser) {
-      if (currentUser.shouldVerifyIdentity) {
-        _showVerifyIdentityDialog(context);
-      }
-    });
-  }
-
-  void _showVerifyIdentityDialog(BuildContext context) {
-    showAlertDialog(context,
-        titleKey: 'identity-not-verified',
-        messageKey: 'identity-verification-alert-message',
-        buttonKey1: 'back',
-        buttonKey2: 'identity-verification-verify-now',
-        buttonAction1: () => Navigator.pop(context),
-        buttonAction2: () {
-          Navigator.pop(context);
-          openRoute(context, VerifyIdentityScreen.ROUTE_NAME);
-        });
+    maybeShowVerificationDialog(context);
   }
 }
