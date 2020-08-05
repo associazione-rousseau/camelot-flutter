d91430bdd830494b82d9b354e7a4cc4c74da06e4
emanuelconunaemme <emanuelconunaemme@hotmail.it>
2020-08-04 19:24:28 -0700

    Remove unused import

--- rousseau_vote/lib/src/screens/polls_screen.dart
+++ rousseau_vote/lib/src/screens/polls_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:rousseau_vote/src/injection/injector_config.dart';
-import 'package:rousseau_vote/src/models/poll.dart';
 import 'package:rousseau_vote/src/models/poll_list.dart';
 import 'package:rousseau_vote/src/network/graphql/graphql_queries.dart';
 import 'package:rousseau_vote/src/prefetch/prefetch_manager.dart';
@@ -13,7 +12,6 @@ import 'package:rousseau_vote/src/widgets/polls_list_widget.dart';
 
 import 'package:rousseau_vote/src/widgets/rousseau_logged_scaffold.dart';
 import 'package:rousseau_vote/src/widgets/rousseau_app_bar.dart';
-import 'package:rousseau_vote/src/widgets/vote/poll_card.dart';
 
 class PollsScreen extends StatelessWidget {
   static const String ROUTE_NAME = '/polls';
