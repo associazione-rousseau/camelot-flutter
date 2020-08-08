8288a1ab46532d95ef2092d1094627fdfb398d0c
emanuelconunaemme <emanuelconunaemme@hotmail.it>
2020-08-07 18:07:40 -0700

    Refactor RousseauAppBar to support white toolbar

--- rousseau_vote/lib/src/widgets/rousseau_app_bar.dart
+++ rousseau_vote/lib/src/widgets/rousseau_app_bar.dart
@@ -5,13 +5,19 @@ import 'package:rousseau_vote/src/config/app_constants.dart';
 
 class RousseauAppBar extends StatelessWidget implements PreferredSizeWidget {
 
+  const RousseauAppBar({ this.white = false });
+
+  final bool white;
+
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: const Image(
-        image: WHITE_LOGO,
+      iconTheme: white ? const IconThemeData(color: PRIMARY_RED) : null,
+      title: Image(
+        image: white ? RED_LOGO : WHITE_LOGO,
         height: 50,
-      )
+      ),
+      backgroundColor: white ? Colors.white : PRIMARY_RED,
     );
   }
 
