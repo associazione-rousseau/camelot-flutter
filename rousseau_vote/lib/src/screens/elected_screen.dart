8acaf4be4ef40dd425d837fab3d9ff96e4ab6fb1
emanuelconunaemme <emanuelconunaemme@hotmail.it>
2020-08-10 20:43:13 -0700

    Show work in progress on ElectedScreen

--- rousseau_vote/lib/src/screens/elected_screen.dart
+++ rousseau_vote/lib/src/screens/elected_screen.dart
@@ -1,5 +1,9 @@
 
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:rousseau_vote/src/l10n/rousseau_localizations.dart';
+import 'package:rousseau_vote/src/util/widget/vertical_space.dart';
+import 'package:rousseau_vote/src/widgets/core/icon_text_screen.dart';
 
 class ElectedScreen extends StatelessWidget {
 
@@ -7,8 +11,13 @@ class ElectedScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      'I miei eletti',
+    final String subtitle = RousseauLocalizations.getText(context, 'elected-coming-soon');
+    return Column(
+      children: [
+        const IconTextScreen(iconData: MdiIcons.accountGroup, messageKey: 'elected-full-title',),
+        const VerticalSpace(50),
+        Text(subtitle),
+      ],
     );
   }
 }
