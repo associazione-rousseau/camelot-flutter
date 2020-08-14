be66a086e28e593e606446f08ebe6c30323ef308
emanuelconunaemme <emanuelconunaemme@hotmail.it>
2020-08-13 22:18:23 -0700

    Add build hash output commit

--- bin/build_all.sh
+++ bin/build_all.sh
@@ -13,5 +13,7 @@ VERSION_NUMBER=$2
 
 echo_colored "Building Version $VERSION_NUMBER ($VERSION_NAME)" $GREEN
 
-bin/build_ios.sh $VERSION_NAME $VERSION_NUMBER
-bin/build_android.sh $VERSION_NAME $VERSION_NUMBER
+bin/build_ios.sh $VERSION_NAME $VERSION_NUMBER && \
+bin/build_android.sh $VERSION_NAME $VERSION_NUMBER && \
+
+# git commit -a -m "Build $VERSION_NAME ($VERSION_NUMBER)"
