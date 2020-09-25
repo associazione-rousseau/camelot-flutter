e7d67d4659e929795057a375a34f6c41e9ac9fe4
emanuelconunaemme <emanuelconunaemme@hotmail.it>
2020-09-24 20:16:11 -0700

    Improve EventDetailsScreen adding location and time

--- rousseau_vote/lib/src/screens/events/event_details_screen.dart
+++ rousseau_vote/lib/src/screens/events/event_details_screen.dart
@@ -3,8 +3,15 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:rousseau_vote/src/config/app_constants.dart';
+import 'package:rousseau_vote/src/l10n/rousseau_localizations.dart';
 import 'package:rousseau_vote/src/models/arguments/event_details_arguments.dart';
 import 'package:rousseau_vote/src/models/events/event.dart';
+import 'package:rousseau_vote/src/models/events/event_date.dart';
+import 'package:rousseau_vote/src/util/ui_util.dart';
+import 'package:rousseau_vote/src/util/widget/horizontal_space.dart';
+import 'package:rousseau_vote/src/util/widget/vertical_space.dart';
 import 'package:rousseau_vote/src/widgets/events/event_picture_placeholder.dart';
 import 'package:rousseau_vote/src/widgets/menu/web_menu_button.dart';
 import 'package:rousseau_vote/src/widgets/rousseau_animated_screen.dart';
@@ -26,7 +33,7 @@ class EventDetailsScreen extends StatelessWidget {
       ),
       extendedAppBar: _header(),
       backgroundColor: Colors.white,
-      body: _body(),
+      body: _body(context),
       actions: [WebMenuButton(url: arguments.event.permalink)],
     );
   }
@@ -41,11 +48,56 @@ class EventDetailsScreen extends StatelessWidget {
     );
   }
 
-  Widget _body() {
+  Widget _body(BuildContext context) {
+    return Card(
+        child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              ListTile(
+                title: Text(
+                  arguments.event.title,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      fontFamily: 'Roboto '),
+                ),
+                subtitle: Text(arguments.event.subtitle),
+              ),
+              const VerticalSpace(10),
+              _infoRow(MdiIcons.mapMarkerOutline, Text(arguments.event.location)),
+              _infoRow(MdiIcons.clockOutline, _timeSection(context)),
+              const VerticalSpace(10),
+              Html(data: arguments.event.description, onLinkTap: (String url) => openUrlExternal(context, url),),
+            ]),
+      );
+  }
+
+  Widget _infoRow(IconData iconData, Widget info) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(iconData, size: 20, color: PRIMARY_RED,),
+          const HorizontalSpace(5),
+          info,
+        ],
+      ),
+    );
+  }
+
+  Widget _timeSection(BuildContext context) {
+    final List<Widget> times = <Widget>[];
+    for(EventDate date in arguments.event.dates) {
+      final String dateString = formatDateDayMonth(context, date.timestamp);
+      final String text = date.end != null && date.end.isNotEmpty ?
+      RousseauLocalizations.getTextFormatted(context, 'event-date', [dateString, date.start, date.end]) :
+      RousseauLocalizations.getTextFormatted(context, 'event-date-no-end', [dateString, date.start]);
+      times.add(Text(text));
+    }
     return Column(
-      children: [
-        Html(data: arguments.event.description),
-      ],
+      children: times,
     );
   }
 
