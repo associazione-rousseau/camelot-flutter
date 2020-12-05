a37b6ff071265a59755d2c545672d14f591d4ded
emanuelconunaemme <emanuelconunaemme@hotmail.it>
2020-12-04 23:49:06 -0800

    Pull out reusable UserCard from CandidateOptionCard

--- rousseau_vote/lib/src/widgets/vote/candidate_option_card.dart
+++ rousseau_vote/lib/src/widgets/vote/candidate_option_card.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:rousseau_vote/src/config/app_constants.dart';
 import 'package:rousseau_vote/src/l10n/rousseau_localizations.dart';
-import 'package:rousseau_vote/src/models/entity.dart';
+import 'package:rousseau_vote/src/models/user.dart';
 import 'package:rousseau_vote/src/models/option.dart';
 import 'package:rousseau_vote/src/providers/vote_options_provider.dart';
 import 'package:rousseau_vote/src/util/profile_util.dart';
@@ -11,6 +11,7 @@ import 'package:rousseau_vote/src/util/ui_util.dart';
 import 'package:rousseau_vote/src/util/widget/vertical_space.dart';
 import 'package:rousseau_vote/src/widgets/profile/badges_widget.dart';
 import 'package:rousseau_vote/src/widgets/user/profile_picture.dart';
+import 'package:rousseau_vote/src/widgets/user/user_card.dart';
 
 class CandidateOptionCard extends StatelessWidget {
   const CandidateOptionCard(this._option, this._selected);
@@ -23,52 +24,7 @@ class CandidateOptionCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final Entity user = _option.entity;
-    final String subtitle = getUserSubtitleShort(
-        context, user.profile.age, user.residence);
-    return Card(
-      shape: RoundedRectangleBorder(
-          side: _selected ? const BorderSide(color: PRIMARY_RED, width: 2.0) : BorderSide.none,
-          borderRadius: BorderRadius.circular(DEFAULT_SPACING)),
-      elevation: 5,
-      child: InkWell(
-        onTap: () => _onTap(context),
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            children: <Widget>[
-              ListTile(
-                  leading: ProfilePicture(url: user.getProfilePictureUrl(), radius: PROFILE_PICTURE_RADIUS),
-                  title: Text(
-                    user.fullName,
-                    style: const TextStyle(fontSize: 20),
-                  ),
-                  subtitle: Text(subtitle),
-              ),
-              BadgesWidget(user.badges, 25),
-              const VerticalSpace(10),
-              _profileButton(context),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _profileButton(BuildContext context) {
-    return ButtonBar(
-      alignment: MainAxisAlignment.center,
-      buttonPadding: const EdgeInsets.all(0),
-      children: <Widget>[
-        FlatButton(
-          child: Text(
-            RousseauLocalizations.getText(context, 'profile').toUpperCase(),
-            style: const TextStyle(fontSize: 15),
-          ),
-          onPressed: openProfileAction(context, _option.entity.slug),
-        ),
-      ],
-    );
+    return UserCard(user: _option.entity, onTap: _onTap, selected: _selected,);
   }
 
   void _onTap(BuildContext context) {
