a5632c6c496f5eb21db1956f3de8dbf268a6b286
emanuelconunaemme <emanuelconunaemme@hotmail.it>
2020-12-20 21:26:19 -0800

    Fix infinite loading of articles carousel if there is an error

--- rousseau_vote/lib/src/providers/blog_search_provider.dart
+++ rousseau_vote/lib/src/providers/blog_search_provider.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:rousseau_vote/src/injection/injector_config.dart';
 import 'package:rousseau_vote/src/models/blog/blog_instant_article.dart';
@@ -7,18 +6,25 @@ import 'package:rousseau_vote/src/network/cache/blog_instant_articles_cache.dart
 import 'package:rousseau_vote/src/network/handlers/blog_instant_article_network_handler.dart';
 
 class BlogSearchProvider extends ChangeNotifier {
-
   bool isLoading = false;
   List<BlogInstantArticle> results;
 
   void searchByAuthor(UserProfile userProfile) {
     isLoading = true;
-    final Future<List<BlogInstantArticle>> future = getIt<BlogInstantArticleNetworkHandler>().getPosts(0, authorRousseauSlug: userProfile.slug);
-    future.then((List<BlogInstantArticle> articles) {
-      results = articles;
-      getIt<BlogInstantArticlesCache>().addInstantArticles(articles);
-      notifyListeners();
-    }).whenComplete(() => isLoading = false);
+    final Future<List<BlogInstantArticle>> future =
+        getIt<BlogInstantArticleNetworkHandler>()
+            .getPosts(0, authorRousseauSlug: userProfile.slug);
+    future
+        .then((List<BlogInstantArticle> articles) {
+          results = articles;
+          getIt<BlogInstantArticlesCache>().addInstantArticles(articles);
+          notifyListeners();
+        })
+        .catchError((Object error) => print(error))
+        .whenComplete(() {
+          isLoading = false;
+          notifyListeners();
+        });
 
     notifyListeners();
   }
